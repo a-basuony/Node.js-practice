e10aa9906878d68518996c180949273b0d6a7afd
Ahmed Basuony <99911074+a-basuony@users.noreply.github.com>
2023-09-13 20:48:55 +0300

    using nodemon and body pareser

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const http = require("http");
+const express = require("express");
+const bodyParsed = require("body-parser");
+const app = express();
+
+// app.use((res, req, next) => {
+//   console.log("first middleware");
+//   next(); // if we don't call this function then the request will not be processed further
+// });
+
+app.use(bodyParsed.urlencoded({ extended: false }));
+
+app.use("/", (req, res, next) => {
+  console.log("this always runs!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' ><button type='submit'>Send</button></input></form>"
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello From Express</h1>");
+});
+
+app.listen(3000);
+
+// -----------------------
+
+// const http = require("http");
 // const routes = require("./routes");
 
 // const server = http.createServer(routes.handler);
 // console.log(routes.someText);
 
-const express = require("express");
-
-const app = express();
-
-app.use((res, req, next) => {
-  console.log("first middleware");
-  next(); // if we don't call this function then the request will not be processed further
-});
-
-app.use((res, req, next) => {
-  console.log("second middle ware");
-  next(); // if we don't call this function then the request will not be processed further
-});
-
-const server = http.createServer(app);
-
-server.listen("3000");
+// const server = http.createServer(app);
+// server.listen("3000");
